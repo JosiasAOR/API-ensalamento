ac828ec903a97fe90117e484aabfd7b55ea2d602
JosiasAOR <josiasaor@gmail.com>
2023-08-19 22:30:04 -0300

    feat(crud-professores) : This code will add the DELETE functionality

--- routes/exibirProfessores.js
+++ routes/exibirProfessores.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/exibirProfessores')
 const controller2 = require('../controllers/cadastrarProfessor')
+const controller3 = require('../controllers/deletarProfessor')
 
 router.get('/exibir', controller.exibirProfessores)
 router.post('/cadastrar', controller2.cadastroProfessores)
+router.delete('/deletar/:id', controller3.deletarProfessor)
 
 
 module.exports = router
